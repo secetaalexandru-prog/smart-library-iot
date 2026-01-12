e66852f42c7b80c2f3123118fae37bc2d3f035bd
secetaalexandru-prog <secetaalexandru@gmail.com>
2026-01-12 14:45:27 +0200

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,18 @@
+-- Baza de date pentru sistemul Smart Library
+
+CREATE TABLE books (
+    book_id SERIAL PRIMARY KEY,
+    title TEXT,
+    author TEXT,
+    status TEXT
+);
+
+CREATE TABLE events (
+    event_id SERIAL PRIMARY KEY,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    tag_uid TEXT,
+    event_type TEXT,
+    device_id TEXT,
+    zone_id TEXT
+);
 
